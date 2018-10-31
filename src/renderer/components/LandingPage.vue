cd72e866aefebb26e2b5e4e1d9d9a2e6465bd9d6
Sreng Shuan <31960170+Sreng-S@users.noreply.github.com>
2018-10-31 11:50:26 +0700

    Update LandingPage.vue

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          MediVRx
         </span>
         <system-information></system-information>
       </div>
